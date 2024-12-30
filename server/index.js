6f311b88955d1b72b6c49f1eb999d9251d6f8d3f
labib <alif16416@gmail.com>
2024-12-31 00:23:41 +0600

    Sign up problems fixed

--- server/index.js
+++ server/index.js
@@ -1,26 +1,48 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB Successfully!");
   })
   .catch((err) => {
-    console.log("Error connecting to MongoDB:", err);
+    console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
-
-app.use('/server/user', userRouter);
-app.use('/server/auth', authRouter);
-
+app.use(cookieParser());
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("Server running on port: 5000");
+});
+
+app.use("/server /user", userRouter);
+app.use("/server/auth", authRouter);
+
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+// ErrorHandler MiddleWare
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
